3f6c43291e61e85ce10c515d3a21cec60e7aef49
juanpablo <juan2224649@correo.uis.edu.co>
2022-09-10 08:30:00 +0200

    Se agrega el procedimiento en el programa

--- calculadora_menu.py
+++ calculadora_menu.py
@@ -1,5 +1,6 @@
 # Calculadora con menú
 from math import log
+from operator import truediv
 
 print("-------------------------")
 print("---CALCULADORA-MENU------")
@@ -20,3 +21,35 @@ print("5. Potencia(El primero a la potencia del el segundo)")
 print("6. Logaritmo(El logartimo del primero)")
 
 opcion = int(input("\nDame la opcion: "))
+# processing
+if(opcion ==1):
+    z = x + y
+    print(x,"+",y)
+elif (opcion ==2):
+    z = x - y
+    print(x,"-",y)
+elif (opcion ==3):
+    z = x * y
+    print(x,"x",y)
+elif (opcion ==4 and y!=0):
+    z = x / y
+    print(x,"/",y)
+elif (opcion ==4 and y==0):
+    print("El donominador el igual a cero y")
+    print("No se puede realizar la division")
+    bandera=True
+elif (opcion ==5):
+    z = pow(x,y)
+    print(x,"^",y)
+elif (opcion ==6 and x > 0):
+    print("logaritmo de",x)
+elif(opcion == 6 and x<=0):
+    print("El valor de x es <= a cero y")
+    print("No se puede calcular el logaritmo.")
+    bandera = True
+else:
+    print ("Opcion no valida")
+
+# se escribe el resultado con otra condicion
+if (opcion < 7 and bandera==False):
+    print("El resultado es",z)
